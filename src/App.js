2cf95078eb7064b02bedf9907faeedb42c877dfb
h_yabiku <>
2022-03-30 22:28:16 +0900

    count up 2

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import React, { useState } from 'react'
 const App = () => {
   const [count, setCount] = useState(0)
 
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
+  const increment = () => setCount(previousCount => previousCount + 1)
+  const decrement = () => setCount(previousCount => previousCount - 1)
+  const reset = () => setCount(0)
 
   return (
     <>
       <div>count: {count}</div>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
+      <button onClick={reset}>リセット</button>
     </>
   )
 }
