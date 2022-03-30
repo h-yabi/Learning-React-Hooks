29f787783aabe05c55349cecdf622d14ea94e52c
h_yabiku <>
2022-03-30 22:08:12 +0900

    prettier 追加

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react"
+import React, { useState } from 'react'
 
 const App = () => {
   const [count, countUp] = useState(0)
-  console.log(count)
   return (
     <div className="App">
-      test
+      {count}
+      <button onClick={countUp}></button>
     </div>
   )
 }
