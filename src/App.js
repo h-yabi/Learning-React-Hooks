604aff774012855c7dac704fbe8f38522769536d
h_yabiku <>
2022-03-31 21:52:13 +0900

    複数の状態を1つのオブジェクトに統合

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import React, { useState } from 'react'
 
 const App = props => {
 
-  const [name, setName] = useState(props.name)
-  const [price, setPrice] = useState(props.price)
-
-  const reset = () => {
-    setName(props.name)
-    setPrice(props.price)
-  }
+  const [state, setState] = useState(props)
+  const { name, price } = state
+  const reset = () => setState(props)
 
   return (
     <>
       <p>現在の{name}は、{price}です。</p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={() => setState({ ...state, price: price + 1 })}>+1</button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>-1</button>
       <button onClick={reset}>Reset</button>
-      <input value={name} type="text" onChange={e => setName(e.target.value)} />
+      <input value={name} type="text" onChange={e => setState({ ...state, name: e.target.value })} />
     </>
   )
 }
