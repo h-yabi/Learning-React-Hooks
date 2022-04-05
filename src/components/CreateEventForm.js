714a82abf20e0e6e38d2ee047f4460f2ea1d3a46
h_yabiku <>
2022-04-05 21:35:32 +0900

    コンポーネントアップデート

--- src/components/CreateEventForm.js
+++ src/components/CreateEventForm.js
@@ -23,18 +23,21 @@ const CreateEventForm = ({ event, dispatch }) => {
   }
 
   return (
-    <form>
-      <div className="form-group">
-        <label htmlFor="formEventTitle">タイトル</label>
-        <input className="form-control" id="formEventTitle" type="text" value={title} onChange={e => setTitle(e.target.value)} />
-      </div>
-      <div className="form-group">
-        <label htmlFor="formEventBody">ボディー</label>
-        <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)} />
-      </div>
-      <button type="button" className="btn btn-primary mr-3" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
-      <button type="button" className="btn btn-danger" onClick={deleteAllEvents} disabled={event.length === 0}>全てのイベントを削除する</button>
-    </form>
+    <>
+      <h4>イベント作成フォーム</h4>
+      <form>
+        <div className="form-group">
+          <label htmlFor="formEventTitle">タイトル</label>
+          <input className="form-control" id="formEventTitle" type="text" value={title} onChange={e => setTitle(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="formEventBody">ボディー</label>
+          <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)} />
+        </div>
+        <button type="button" className="btn btn-primary mr-3" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
+        <button type="button" className="btn btn-danger" onClick={deleteAllEvents} disabled={event.length === 0}>全てのイベントを削除する</button>
+      </form>
+    </>
   )
 }
 
--- src/components/Events.js
+++ src/components/Events.js
@@ -9,36 +9,39 @@ const Events = ({ event, dispatch }) => {
   }
 
   return (
-    <table className="table table-hover">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>タイトル</th>
-          <th>ボディー</th>
-          <th>削除</th>
-        </tr>
-      </thead>
-      <tbody>
-        {event.map((event, index) => {
-          return (
-            <tr key={index}>
-              <td>{event.id}</td>
-              <td>{event.title}</td>
-              <td>{event.body}</td>
-              <td>
-                <button
-                  type="button"
-                  className="btn btn-danger"
-                  onClick={() => deleteEvent(event.id)}
-                >
-                  削除
-                </button>
-              </td>
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
+    <>
+      <h4 className="mt-5">イベント一覧</h4>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>タイトル</th>
+            <th>ボディー</th>
+            <th>削除</th>
+          </tr>
+        </thead>
+        <tbody>
+          {event.map((event, index) => {
+            return (
+              <tr key={index}>
+                <td>{event.id}</td>
+                <td>{event.title}</td>
+                <td>{event.body}</td>
+                <td>
+                  <button
+                    type="button"
+                    className="btn btn-danger"
+                    onClick={() => deleteEvent(event.id)}
+                  >
+                    削除
+                  </button>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </>
   )
 }
 
